bb9736309fb3d486893151079a38959ceb020e97
FEI YUAN <fei.yuan@bcm.edu>
2021-04-13 17:06:29 -0500

    Refactored overlap_peak.pl

--- source/overlap_peak.pl
+++ source/overlap_peak.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/env perl
 
-# Adopted from https://github.com/YeoLab/eclip/blob/master/bin/overlap_peakfi_with_bam_PE.pl
+# Adopted from https://github.com/YeoLab/eclip/blob/master/bin/overlap_peak_bed_with_bam_PE.pl
 
 use warnings;
 use strict;
@@ -10,41 +10,22 @@ use Statistics::R;
 my $R = Statistics::R->new();
 
 unless ($ARGV[0] && $ARGV[1] && $ARGV[2] && $ARGV[3] && $ARGV[4]) {
-    print STDERR "usage: perl overlap_peakfi_with_bam_PE.pl Experiment_bam_file Input_bam_file Peak_file Mapped_ip_read_num_file Mapped_input_read_num_file Output_file\n\n";
+    print STDERR "usage: perl overlap_peak.pl IP_bam INPUT_bam peak_bed ip_mapped_read_number input_mapped_read_number output_file\n\n";
     exit;
 }
 
-
-my $exptbamfile = $ARGV[0];
-my $inputbamfile = $ARGV[1];
-my $peakfi = $ARGV[2];
-
-my %precalculated_fisher;
-my @fi1array = split(/\//,$exptbamfile);
-my @input_fiarray = split(/\//,$inputbamfile);
+my $ip_bam = $ARGV[0];
+my $input_bam = $ARGV[1];
+my $peak_bed = $ARGV[2];
 my %mapped_read_count;
+$mapped_read_count{"ip"} = $ARGV[3];
+$mapped_read_count{"input"} = $ARGV[4];
+my $output = $ARGV[5];
 
-my $expt_readnum_file = $ARGV[3];
-my $input_readnum_file = $ARGV[4];
+my $verbose = 0;
+my %precalculated_fisher;
 
-open(RN,$expt_readnum_file) || die "no $expt_readnum_file\n";
-for my $line (<RN>) {
-    chomp($line);
-    next unless ($line);
-    $mapped_read_count{"expt"} = $line;
-}
-close(RN);
-
-# my $read_num_fi = $ARGV[3];
-open(RN,$input_readnum_file) || die "no $input_readnum_file\n";
-for my $line (<RN>) {
-    chomp($line);
-    next unless ($line);
-    $mapped_read_count{"input"} = $line;
-}
-close(RN);
-
-unless (exists $mapped_read_count{"expt"}) {
+unless (exists $mapped_read_count{"ip"}) {
     print STDERR "Fatal error: missing experimental read count\n";
     exit;
 }
@@ -56,35 +37,32 @@ unless (exists $mapped_read_count{"input"}) {
 my %peaks;
 my $hashing_value = 100000;
 my %peak_read_counts;
-&read_peakfi($peakfi);
-#print STDERR "num of peaks identified: ".scalar(keys %peak_read_counts)."\n";
-&read_bamfi($exptbamfile,"expt");
-&read_bamfi($inputbamfile,"input");
+&read_peak_bed($peak_bed);
+&read_bam($ip_bam,"ip");
+&read_bam($input_bam,"input");
 
-my $output_fi = $ARGV[5];
-
-my $fisher_tmp_fi = $output_fi.".tmp_fisher";
-my $fisher_tmp_fi_out = $output_fi.".tmp_fisher.out";
-
-my $output_full = $output_fi.".full";
+my $output_full = $output;
+$output_full =~ s/\.bed/.full.bed/;
+open(OUT,">$output");
 open(OUTFULL,">$output_full");
-open(OUT,">$output_fi");
 
 for my $peak (keys %peak_read_counts) {
-    unless (exists $peak_read_counts{$peak}{"expt"}) {
-	$peak_read_counts{$peak}{"expt"} = 1;
+    unless (exists $peak_read_counts{$peak}{"ip"}) {
+	    $peak_read_counts{$peak}{"ip"} = 1;
     }
     $peak_read_counts{$peak}{"input"}++;
-    my $l2fc = log(($peak_read_counts{$peak}{"expt"}/$mapped_read_count{"expt"}) / ($peak_read_counts{$peak}{"input"}/$mapped_read_count{"input"})) / log(2);
-    my ($chipval,$chival,$chitype,$chienrdepl) = &fisher_or_chisq($peak_read_counts{$peak}{"expt"},$mapped_read_count{"expt"}-$peak_read_counts{$peak}{"expt"},$peak_read_counts{$peak}{"input"},$mapped_read_count{"input"}-$peak_read_counts{$peak}{"input"});
-    my $log10pval = $chipval > 0 ? -1 * log($chipval)/log(10) : 400 ;
-
-    my ($chr,$pos,$str,$origpval) = split(/\:/,$peak);
-    my ($start,$stop) = split(/\-/,$pos);
-    print OUT "$chr\t$start\t$stop\t$log10pval\t$l2fc\t$str\n";
-    print OUTFULL "$chr\t$start\t$stop\t$peak\t".$peak_read_counts{$peak}{"expt"}."\t".$peak_read_counts{$peak}{"input"}."\t$chipval\t$chival\t$chitype\t$chienrdepl\t$log10pval\t$l2fc\n";
-
-
+    my $l2fc = log(($peak_read_counts{$peak}{"ip"}/$mapped_read_count{"ip"}) /
+               ($peak_read_counts{$peak}{"input"}/$mapped_read_count{"input"})) / log(2);
+    my ($p,$v,$method,$status) = &fisher_or_chi($peak_read_counts{$peak}{"ip"},
+                                                $mapped_read_count{"ip"}-$peak_read_counts{$peak}{"ip"},
+                                                $peak_read_counts{$peak}{"input"},
+                                                $mapped_read_count{"input"}-$peak_read_counts{$peak}{"input"});
+    my $l10p = $p > 0 ? -1 * log($p)/log(10) : 400 ;
+    my ($chrom, $position, $strand, $p_value) = split(/\:/, $peak);
+    my ($start, $stop) = split(/\-/, $position);
+    print OUT "$chrom\t$start\t$stop\t$l10p\t$l2fc\t$strand\n";
+    print OUTFULL "$chrom\t$start\t$stop\t$peak\t".$peak_read_counts{$peak}{"ip"}."\t"
+                  .$peak_read_counts{$peak}{"input"}."\t$p\t$v\t$method\t$status\t$l10p\t$l2fc\n";
 }
 close(OUT);
 
@@ -108,113 +86,70 @@ sub min {
     }
 }
 
-
-sub fisher_or_chisq {
-    my ($a,$b,$c,$d) = @_;
+sub fisher_or_chi {
+    my ($a, $b, $c, $d) = @_;
     unless ($a && $b && $c && $d) {
     }
-
     my $tot = $a + $b + $c + $d;
-    my $expa = ($a+$c)*($a+$b)/$tot;
-    my $expb = ($b+$d)*($a+$b)/$tot;
-    my $expc = ($a+$c)*($c+$d)/$tot;
-    my $expd = ($b+$d)*($c+$d)/$tot;
+    my $exp_a = ($a+$c)*($a+$b)/$tot;
+    my $exp_b = ($b+$d)*($a+$b)/$tot;
+    my $exp_c = ($a+$c)*($c+$d)/$tot;
+    my $exp_d = ($b+$d)*($c+$d)/$tot;
 
     my $direction = "enriched";
-    if ($a<$expa) {
+    if ($a < $exp_a) {
         $direction = "depleted";
         return(1,"DEPL","N",$direction);
     }
 
-    if ($expa < 5 || $expb < 5 || $expc < 5 || $expd < 5 || $a < 5 || $b < 5 || $c < 5 || $d < 5) {
+    if ($exp_a < 5 || $exp_b < 5 || $exp_c < 5 || $exp_d < 5 || $a < 5 || $b < 5 || $c < 5 || $d < 5) {
         if (exists $precalculated_fisher{$a."|".$c}) {
-            return($precalculated_fisher{$a."|".$c}{p},$precalculated_fisher{$a."|".$c}{v},"F",$direction);
+            return($precalculated_fisher{$a."|".$c}{p}, $precalculated_fisher{$a."|".$c}{v}, "F", $direction);
         } else {
-            my ($pval,$val) = &fisher_exact($a,$b,$c,$d);
-            $precalculated_fisher{$a."|".$c}{p} = $pval;
-            $precalculated_fisher{$a."|".$c}{v} = $val;
-            return($pval,$val,"F",$direction);
+            my ($p,$v) = &fisher_exact($a,$b,$c,$d);
+            $precalculated_fisher{$a."|".$c}{p} = $p;
+            $precalculated_fisher{$a."|".$c}{v} = $v;
+            return($p, $v, "F", $direction);
         }
     } else {
-        my ($pval,$val) = &chi_square($a,$b,$c,$d);
-        return($pval,$val,"C",$direction);
+        my ($p, $v) = &chi_square($a, $b, $c, $d);
+        return($p, $v,"C", $direction);
     }
 }
+
 sub chi_square {
-    my ($a,$b,$c,$d) = @_;
+    my ($a, $b, $c, $d) = @_;
     return(0) unless ($a && $b && $c && $d);
     my $tot = $a + $b + $c + $d;
-    my $expa = ($a+$c)*($a+$b)/$tot;
-    my $expb = ($b+$d)*($a+$b)/$tot;
-    my $expc = ($a+$c)*($c+$d)/$tot;
-    my $expd = ($b+$d)*($c+$d)/$tot;
+    my $exp_a = ($a+$c)*($a+$b)/$tot;
+    my $exp_b = ($b+$d)*($a+$b)/$tot;
+    my $exp_c = ($a+$c)*($c+$d)/$tot;
+    my $exp_d = ($b+$d)*($c+$d)/$tot;
 
-    if ($expa >= 5 || $expb >= 5 || $expc >= 5 || $expd >= 5) {
-        my $chival = &square(&abs($a-$expa)-0.5)/$expa +  &square(&abs($b-$expb)-0.5)/$expb + &square(&abs($c-$expc)-0.5)/$expc +  &square(&abs($d-$expd)-0.5)/$expd;
-
-        my $pval = Statistics::Distributions::chisqrprob(1,&abs($chival));
-
-        if ($a<$expa) {
-            $chival = $chival * -1;
+    if ($exp_a >= 5 || $exp_b >= 5 || $exp_c >= 5 || $exp_d >= 5) {
+        my $v = &square(&abs($a-$exp_a) - 0.5) / $exp_a +
+                &square(&abs($b - $exp_b)-0.5) / $exp_b +
+                &square(&abs($c - $exp_c)-0.5) / $exp_c +
+                &square(&abs($d - $exp_d)-0.5) / $exp_d;
+        my $p = Statistics::Distributions::chisqrprob(1,&abs($v));
+        if ($a < $exp_a) {
+            $v = $v * -1;
         }
-        return ($pval,$chival);
+        return ($p, $v);
     } else {
-         #       print "\n";
-        print STDERR "shouldn't get to this - should have been shunted into fisher exact test\n";
+        print STDERR "ERROR: shouldn't get to this - should have been shunted into fisher exact test\n";
         return(1);
     }
 }
 
 sub fisher_exact {
-    my ($x1,$x2,$y1,$y2) = @_;
-    #Run fisher exact test in R
-
+    my ($x1, $x2, $y1, $y2) = @_;
     $R->run("rm(list = ls())");
-    $R->run("blah <- matrix(c(".$x1.",".$x2.",".$y1.",".$y2."),nrow=2)");
+    $R->run("blah <- matrix(c(".$x1.",".$x2.",".$y1.",".$y2."), nrow=2)");
     $R->run("foo <- fisher.test(blah)");
-    my $p_value_vs_bgd = $R->get('foo$p.value');
-    my $val = "F";
-    return($p_value_vs_bgd,$val);
-}
-
-sub fisher_exact_backup {
-    my ($x1,$x2,$y1,$y2) = @_;
-    system("rm $fisher_tmp_fi_out");
-    system("rm $fisher_tmp_fi");
-    my $fisher_tmp_fi_s = $fisher_tmp_fi.".s";
-
-    system("rm $fisher_tmp_fi_s");
-
-    open(LISTA,">$fisher_tmp_fi");
-    print LISTA "".$x1."\t".$x2."\t".$y1."\t".$y2."\n";
-    close(LISTA);
-
-
-    my $p_value_vs_bgd = "";
-
-    while (length($p_value_vs_bgd) < 1) {
-	open(RFI,">$fisher_tmp_fi_s");
-	printf RFI "A <- scan\(\"".$fisher_tmp_fi."\"\)\n";
-	printf RFI "blah <- matrix(c(A),nrow=2)\n";
-	printf RFI "sink(\"".$fisher_tmp_fi_out."\")\n";
-	printf RFI "fisher.test(blah)\n";
-	printf RFI "sink()\n";
-	system("  R --save --quiet < $fisher_tmp_fi_s");
-
-
-	open(FISH,$fisher_tmp_fi_out);
-	for my $line (<FISH>) {
-	    chomp($line);
-	    if ($line =~ /p\-value\s\=\s(\S+)$/ || $line =~ /p\-value\s\<\s(\S+)$/) {
-		$p_value_vs_bgd = $1;
-	    }
-
-	}
-	close(FISH);
-    }
-    my $val = "F";
-    return($p_value_vs_bgd,$val);
-
+    my $p = $R->get('foo$p.value');
+    my $v = "F";
+    return($p, $v);
 }
 
 sub abs {
@@ -222,7 +157,7 @@ sub abs {
     if ($x > 0) {
         return($x);
     } else {
-        return(-1*$x);
+        return(-1 * $x);
     }
 }
 
@@ -231,64 +166,54 @@ sub square {
     return($x * $x);
 }
 
-
-sub read_bamfi {
-    my $bamfile = shift;
+sub read_bam {
+    my $bam = shift;
     my $label = shift;
-    print STDERR "now doing $label $bamfile\n";
-    if ($bamfile =~ /\.bam/) {
-	open(B,"samtools view -h $bamfile |") || die "no $bamfile\n";
-    } elsif ($bamfile =~ /\.sam/) {
-	open(B,$bamfile) || die "no sam $bamfile\n";
+    print STDERR "Reading $label $bam\n" if ($verbose == 1);
+    if ($bam =~ /\.bam/) {
+	    open(B,"samtools view -h $bam |") || die "no $bam\n";
+    } elsif ($bam =~ /\.sam/) {
+	    open(B,$bam) || die "no sam $bam\n";
     } else {
-	print STDERR "file format error not .sam or .bam \n";
-	exit;
+	    print STDERR "ERROR: file format error, file does not end with either .sam or .bam!\n";
+	    exit;
     }
     while (<B>) {
 	my $r1 = $_;
-
 	if ($r1 =~ /^\@/) {
 	    next;
 	}
-	my @tmp_r1 = split(/\t/,$r1);
-	my @read_name = split(/\:/,$tmp_r1[0]);
-	my $randommer = $read_name[0];
+	my @fields = split(/\t/, $r1);
+	my $flag = $fields[1];
+	my $chrom = $fields[2];
+	my $start = $fields[3];
+    my $cigar = $fields[5];
 
-	my $r1_cigar = $tmp_r1[5];
-	my $r1sam_flag = $tmp_r1[1];
-	my $mismatch_flags = $tmp_r1[14];
-
-	my $r1_chr = $tmp_r1[2];
-	my $r1_start = $tmp_r1[3];
-
-	my $frag_strand;
-    if ($r1sam_flag == 147 || $r1sam_flag == 16) {
-       $frag_strand = "-";
-    } elsif ($r1sam_flag == 163 || $r1sam_flag == 0) {
-       $frag_strand = "+";
+	my $strand;
+    if ($flag == 147 || $flag == 16) {
+       $strand = "-";
+    } elsif ($flag == 163 || $flag == 0) {
+       $strand = "+";
     } else {
-       print STDERR "R1 strand error $r1sam_flag\n";
+       print STDERR "R1 strand error, unexpected flag $flag\n";
     }
 
-	my @read_regions = &parse_cigar_string($r1_start,$r1_cigar,$r1_chr,$frag_strand);
+	my @read_regions = &parse_cigar_string($start, $cigar, $chrom, $strand);
 
 	my %tmp_hash;
 	for my $region (@read_regions) {
-	    my ($rchr,$rstr,$rpos) = split(/\:/,$region);
-	    my ($rstart,$rstop) = split(/\-/,$rpos);
-
-	    my $verbose_flag = 0;
-
-	    my $rx = int($rstart / $hashing_value);
-	    my $ry = int($rstop  / $hashing_value);
-	    for my $ri ($rx..$ry) {
-
-		for my $peak (@{$peaks{$rchr}{$rstr}{$ri}}) {
-		    my ($pchr,$ppos,$pstr,$ppval) = split(/\:/,$peak);
-		    my ($pstart,$pstop) = split(/\-/,$ppos);
-		    next if ($pstart >= $rstop || $pstop <= $rstart);
-		    $tmp_hash{$peak} = 1;
-		}
+	    my ($r_chrom, $r_strand, $r_position) = split(/\:/, $region);
+	    my ($r_start, $r_stop) = split(/\-/, $r_position);
+	    my $rx = int($r_start / $hashing_value);
+	    my $ry = int($r_stop  / $hashing_value);
+	    for my $i ($rx..$ry) {
+            for my $peak (@{$peaks{$r_chrom}{$r_strand}{$i}}) {
+                my ($p_chr, $p_pos, $p_str, $p) = split(/\:/, $peak);
+                my ($p_start, $p_stop) = split(/\-/, $p_pos);
+                # my ($p_start, $p_stop) = split(/\-/, split(/\:/, $peak)[1]);
+                next if ($p_start >= $r_stop || $p_stop <= $r_start);
+                $tmp_hash{$peak} = 1;
+            }
 	    }
 	}
 
@@ -302,16 +227,15 @@ sub read_bamfi {
 sub parse_cigar_string {
     my $region_start_pos = shift;
     my $flags = shift;
-    my $chr = shift;
+    my $chrom = shift;
     my $strand = shift;
 
     my $current_pos = $region_start_pos;
     my @regions;
 
     while ($flags =~ /(\d+)([A-Z])/g) {
-
         if ($2 eq "N") {
-            push @regions,$chr.":".$strand.":".($region_start_pos-1)."-".($current_pos-1);
+            push @regions,$chrom.":".$strand.":".($region_start_pos-1)."-".($current_pos-1);
             $current_pos += $1;
             $region_start_pos = $current_pos;
         } elsif ($2 eq "M") {
@@ -325,43 +249,41 @@ sub parse_cigar_string {
             $current_pos += $1;
             #read has deletion relative to genome; genome position has to increase
         } else {
-            print STDERR "flag $1 $2 $flags\n";
+            print STDERR "ERROR: unexpected operator and length: $1 and $2 in cigar string $flags.\n";
 
         }
     }
-    push @regions,$chr.":".$strand.":".($region_start_pos-1)."-".($current_pos-1);
+    push @regions,$chrom.":".$strand.":".($region_start_pos-1)."-".($current_pos-1);
     return(@regions);
 }
 
-sub read_peakfi {
-    my $fi = shift;
-    my $duplicate = 0;
-    my $lc=0;
-    print STDERR "reading peak file $fi\n";
-    open(F,$fi) || die "no peak file $fi\n";
+sub read_peak_bed {
+    my $bed = shift;
+    print STDERR "Reading peak file $bed\n" if ($verbose == 1);
+    open(F, $bed) || die "Cannot open peak file $bed\n";
     for my $line (<F>) {
-	chomp($line);
-	my @tmp = split(/\t/,$line);
-	my $chr = shift(@tmp);
-	my $start = shift(@tmp);
-	my $stop = shift(@tmp);
-	my $gene = shift(@tmp);
-	my $pval = shift(@tmp);
-	my $strand = shift(@tmp);
+        chomp($line);
+        my @fields = split(/\t/,$line);
+        my $chrom = shift(@fields);
+        my $start = shift(@fields);
+        my $stop = shift(@fields);
+        my $gene = shift(@fields);
+        my $p = shift(@fields);
+        my $strand = shift(@fields);
 
-	print STDERR "error - missing start stop $line\n" unless ($start && $stop);
-	next unless ($start && $stop);
+        print STDERR "ERROR: missing start stop in $line\n" unless ($start && $stop);
+        next unless ($start && $stop);
 
-	my $x = int($start / $hashing_value);
-	my $y = int($stop  / $hashing_value);
+        my $x = int($start / $hashing_value);
+        my $y = int($stop  / $hashing_value);
 
-	my $peak = $chr.":".$start."-".$stop.":".$strand.":".$pval;
-	$peak_read_counts{$peak}{allpeaks} = 1;
+        my $peak = $chrom.":".$start."-".$stop.":".$strand.":".$p;
+        $peak_read_counts{$peak}{allpeaks} = 1;
 
 
-	for my $i ($x..$y) {
-	    push @{$peaks{$chr}{$strand}{$i}},$chr.":".$start."-".$stop.":".$strand.":".$pval;
-	}
+        for my $i ($x..$y) {
+            push @{$peaks{$chrom}{$strand}{$i}},$chrom.":".$start."-".$stop.":".$strand.":".$p;
+        }
     }
     close(F);
 }
