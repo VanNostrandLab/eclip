344b7259d45332566e459c17a86682b79e98da44
FEI YUAN <fei.yuan@bcm.edu>
2021-05-26 13:34:05 -0500

    Fixed some bugs in fastd.py

--- source/fastd.py
+++ source/fastd.py
@@ -9,6 +9,8 @@ import sys
 import gzip
 import argparse
 import re
+# from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
+# from functools import partial
 
 from Bio.SeqIO.QualityIO import FastqGeneralIterator
 
@@ -16,6 +18,7 @@ parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('--fastqs', nargs='+', help='Path to the first read (r1) and second read (r2) fastq files.')
 parser.add_argument('--barcodes', nargs='+', help='Names of the barcodes.')
 parser.add_argument('--basename', type=str, help='Basename of the output file.')
+parser.add_argument('--cores', type=int, help='Number of CPU cores can be used.', default=8)
 parser.add_argument('--randomer_length', type=int, help='Length of the randomer, default: 5.', default=5)
 parser.add_argument('--allow_mismatch', type=int, help='Maximum number of base allowed for barcode mismatching.',
                     default=1)
@@ -27,11 +30,12 @@ args = parser.parse_args()
 try:
     from loguru import logger
     logger.remove()
-    logger.add(sys.stderr, level="DEBUG",
+    logger.add(sys.stderr, level="DEBUG" if args.verbose else "INFO",
                format="<light-green>[{time:HH:mm:ss}]</light-green> <level>{message}</level>")
 except ImportError:
     import logging
-    logging.basicConfig(format='[%(asctime)s] %(message)s', datefmt='%H:%M:%S', level=logging.DEBUG)
+    logging.basicConfig(format='[%(asctime)s] %(message)s', datefmt='%H:%M:%S',
+                        level=logging.DEBUG if args.verbose else logging.INFO)
     logger = logging.getLogger()
 
 barcodes_dict = {'A01': 'AAGCAAT',
@@ -74,17 +78,18 @@ def hamming(key, barcode, seq, allow_mismatch=0):
     return (key, len(barcode), mismatch) if mismatch <= allow_mismatch else None
 
 
-def barcoding(read, pattern=None, max_barcode_length=0, randomer_length=0, allow_mismatch=0):
+def barcode_count(read, pattern):
+    pass
+
+
+def barcoding(read, max_barcode_length=0, randomer_length=0, allow_mismatch=0):
     (name1, seq1, quality1), (name2, seq2, quality2) = read
     n1, n2 = name1.split()[0], name2.split()[0]
     assert n1 == n2, ValueError(f'Paired-End reads have mismatch names: {n1} != {n2}')
-    if pattern:
-        matches = list(re.finditer(pattern, seq1[:max_barcode_length]))
-        matches = [(k, len(v), 0) for k, v in matches[0].groupdict().items() if v] if matches else []
-    else:
-        matches = (hamming(key, barcode, seq1[:max_barcode_length], allow_mismatch=allow_mismatch)
-                   for key, barcode in barcodes_dict.items())
-        matches = [match for match in matches if match]
+
+    matches = (hamming(key, barcode, seq1[:max_barcode_length], allow_mismatch=allow_mismatch)
+               for key, barcode in barcodes_dict.items())
+    matches = [match for match in matches if match]
     if matches:
         barcode, barcode_length, _ = sorted(matches, key=lambda x: x[2])[0]
     else:
@@ -96,8 +101,7 @@ def barcoding(read, pattern=None, max_barcode_length=0, randomer_length=0, allow
 
 def fastd():
     (fastq1, fastq2), barcodes = args.fastqs, args.barcodes
-    if args.verbose:
-        logger.debug(f'Demultiplexing {fastq1} and {fastq2} with barcodes {" and ".join(args.barcodes)} ...')
+    logger.debug(f'Demultiplexing {fastq1} and {fastq2} with barcodes {" and ".join(args.barcodes)} ...')
     if barcodes[0] == 'NIL':
         barcodes = barcodes_dict
         pattern = re.compile(fr'{"|".join([f"(?P<{k}>^{v})" for k, v in barcodes_pattern.items()])}')
@@ -110,22 +114,30 @@ def fastd():
                              opener(f'{args.basename}.{barcode}.r2.fastq{".gz" if args.gz else ""}', 'w'))
                    for barcode in barcodes}
 
-    max_barcode_length, n = max([len(v) for v in barcodes.values()]), 0
+    max_barcode_length = max([len(v) for v in barcodes.values()])
     pattern = None if args.allow_mismatch else pattern
+
+    # f1, f2 = FastqGeneralIterator(opener(fastq1, 'r')), FastqGeneralIterator(opener(fastq2, 'r'))
+    # with ProcessPoolExecutor(max_workers=args.cores) as executor:
+    #     reads = executor.map(partial(barcoding, max_barcode_length=max_barcode_length,
+    #                          randomer_length=args.randomer_length, allow_mismatch=args.allow_mismatch),
+    #                          zip(f1, f2), chunksize=400000)
+    #     for barcode, read1, read2 in reads:
+    #         if barcode in writers:
+    #             writer1, writer2 = writers[barcode]
+    #             writer1.write(read1)
+    #             writer2.write(read2)
     for r1, r2 in zip(FastqGeneralIterator(opener(fastq1, 'r')), FastqGeneralIterator(opener(fastq2, 'r'))):
-        barcode, read1, read2 = barcoding((r1, r2), pattern=pattern,
+        barcode, read1, read2 = barcoding((r1, r2),
                                           max_barcode_length=max_barcode_length,
                                           randomer_length=args.randomer_length,
                                           allow_mismatch=args.allow_mismatch)
         if barcode in writers:
-            n += 1
             writer1, writer2 = writers[barcode]
             writer1.write(read1)
             writer2.write(read2)
     _ = [[v[0].close(), v[1].close()] for v in writers.values()]
-    if args.verbose:
-        logger.debug(f'Demultiplexing {fastq1} and {fastq2} with barcodes {" and ".join(args.barcodes)} complete.')
-        logger.debug(f'Successfully saved {n:,} paired reads.')
+    logger.debug(f'Demultiplexing {fastq1} and {fastq2} with barcodes {" and ".join(args.barcodes)} complete.')
 
 
 if __name__ == '__main__':
